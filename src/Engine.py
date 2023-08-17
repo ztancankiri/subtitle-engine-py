79b61ceb2299ce8242799b4c0de9aec727d0d7e6
ztancankiri <ztan.cankiri@gmail.com>
2023-08-18 00:25:10 +0200

    engine fixed

--- src/Engine.py
+++ src/Engine.py
@@ -37,6 +37,20 @@ def extractTrSrtFromMkv(file):
     return extractSrtByLangFromMKV(file, "tr")
 
 
+def initialTask(file):
+    if (checkEnSrt(file)):
+        checkAndFixLocalSubFileName(file, "en")
+    else:
+        extractEnSrtFromMkv(file)
+
+    if (checkTrSrt(file)):
+        checkAndFixLocalSubFileName(file, "tr")
+    else:
+        extractTrSrtFromMkv(file)
+
+    return True
+
+
 def getEnSrtByHash(file):
     opensub = OpenSubtitles()
     token = opensub.login()
@@ -165,7 +179,28 @@ def subsyncTrSrtWithEnSrt(file):
     subPath = findLocalSub(file, subLang)
 
     syncedSubFile = sync(subPath, subLang, refPath, refLang)
-    return checkAndFixLocalSubFileName(syncedSubFile, subLang)
+    if (checkAndFixLocalSubFileName(syncedSubFile, subLang)):
+        return True
+    else:
+        if (os.path.exists(syncedSubFile)):
+            os.unlink(syncedSubFile)
+        return False
+
+
+def subsyncEnSrtWithTrSrt(file):
+    refLang = "tr"
+    subLang = "en"
+
+    refPath = findLocalSub(file, refLang)
+    subPath = findLocalSub(file, subLang)
+
+    syncedSubFile = sync(subPath, subLang, refPath, refLang)
+    if (checkAndFixLocalSubFileName(syncedSubFile, subLang)):
+        return True
+    else:
+        if (os.path.exists(syncedSubFile)):
+            os.unlink(syncedSubFile)
+        return False
 
 
 def subsyncEnSrtWithAudio(file):
@@ -176,7 +211,12 @@ def subsyncEnSrtWithAudio(file):
     subPath = findLocalSub(file, subLang)
 
     syncedSubFile = sync(subPath, subLang, refPath, refLang)
-    return checkAndFixLocalSubFileName(syncedSubFile, subLang)
+    if (checkAndFixLocalSubFileName(syncedSubFile, subLang)):
+        return True
+    else:
+        if (os.path.exists(syncedSubFile)):
+            os.unlink(syncedSubFile)
+        return False
 
 
 def subsyncTrSrtWithAudio(file):
@@ -187,7 +227,12 @@ def subsyncTrSrtWithAudio(file):
     subPath = findLocalSub(file, subLang)
 
     syncedSubFile = sync(subPath, subLang, refPath, refLang)
-    return checkAndFixLocalSubFileName(syncedSubFile, subLang)
+    if (checkAndFixLocalSubFileName(syncedSubFile, subLang)):
+        return True
+    else:
+        if (os.path.exists(syncedSubFile)):
+            os.unlink(syncedSubFile)
+        return False
 
 
 def readyEnSrt(file):
@@ -211,34 +256,62 @@ def readyNothing(file):
 
 
 def runEngine(file):
-    node1 = Node(checkEnSrt, file)
-    node2 = Node(getTrSrtByHash, file)
-    node3 = Node(readyTrSrtAndEnSrt, file)
-    node4 = Node(subsyncTrSrtWithEnSrt, file)
-    node5 = Node(getTrSrtByName, file)
-    node6 = Node(readyEnSrt, file)
-    node7 = Node(getEnSrtByHash, file)
-    node8 = Node(subsyncEnSrtWithAudio, file)
-    node9 = Node(getEnSrtByName, file)
-    node10 = Node(getTrSrtByHash, file)
-    node11 = Node(getTrSrtByName, file)
-    node12 = Node(readyNothing, file)
-    node13 = Node(readyTrSrt, file)
-    node14 = Node(subsyncTrSrtWithAudio, file)
+    node1 = Node(initialTask, file)
+    node2 = Node(checkEnSrt, file)
+    node3 = Node(checkTrSrt, file)
+    node4 = Node(readyTrSrtAndEnSrt, file)
+    node5 = Node(getTrSrtByHash, file)
+    node6 = Node(subsyncTrSrtWithEnSrt, file)
+    node7 = Node(getTrSrtByName, file)
+    node8 = Node(readyEnSrt, file)
+    node9 = Node(getEnSrtByHash, file)
+    node10 = Node(checkTrSrt, file)
+    node11 = Node(getTrSrtByHash, file)
+    node12 = Node(getTrSrtByName, file)
+    node13 = Node(getEnSrtByName, file)
+    node14 = Node(subsyncTrSrtWithEnSrt, file)
+    node15 = Node(checkTrSrt, file)
+    node16 = Node(checkTrSrt, file)
+    node17 = Node(subsyncEnSrtWithTrSrt, file)
+    node18 = Node(readyTrSrt, file)
+    node19 = Node(getTrSrtByHash, file)
+    node20 = Node(getTrSrtByHash, file)
+    node21 = Node(subsyncTrSrtWithAudio, file)
+    node22 = Node(getTrSrtByName, file)
+    node23 = Node(subsyncEnSrtWithAudio, file)
+    node24 = Node(readyNothing, file)
+    node25 = Node(getTrSrtByName, file)
+    node26 = Node(subsyncTrSrtWithEnSrt, file)
+    node27 = Node(getTrSrtByName, file)
+    node28 = Node(subsyncTrSrtWithAudio, file)
 
-    node1.setNexts(node2, node7)
-    node2.setNexts(node3, node5)
-    node3.setNexts(None, None)
-    node4.setNexts(node3, None)
-    node5.setNexts(node4, node6)
-    node6.setNexts(None, None)
-    node7.setNexts(node2, node9)
-    node8.setNexts(node2, None)
-    node9.setNexts(node8, node10)
-    node10.setNexts(node13, node11)
-    node11.setNexts(node14, node12)
-    node12.setNexts(None, None)
-    node13.setNexts(None, None)
-    node14.setNexts(node13, None)
+    node1.setNexts(node2, None)
+    node2.setNexts(node3, node9)
+    node3.setNexts(node4, node5)
+    node4.setNexts(None, None)
+    node5.setNexts(node4, node7)
+    node6.setNexts(node4, None)
+    node7.setNexts(node6, node8)
+    node8.setNexts(None, None)
+    node9.setNexts(node10, node13)
+    node10.setNexts(node4, node11)
+    node11.setNexts(node4, node12)
+    node12.setNexts(node14, node8)
+    node13.setNexts(node16, node15)
+    node14.setNexts(node4, None)
+    node15.setNexts(node18, node19)
+    node16.setNexts(node17, node20)
+    node17.setNexts(node4, node18)
+    node18.setNexts(None, None)
+    node19.setNexts(node18, node22)
+    node20.setNexts(node17, node23)
+    node21.setNexts(node18, node24)
+    node22.setNexts(node21, node24)
+    node23.setNexts(node25, None)
+    node24.setNexts(None, None)
+    node25.setNexts(node26, node8)
+    node26.setNexts(node4, node8)
+    node27.setNexts(node28, node24)
+    node28.setNexts(node18, node24)
 
     node1.run()
